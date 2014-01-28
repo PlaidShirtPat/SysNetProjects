7c590df91dd24361a6500f66c91b2c79f039e18d
Patrick Brown <pjb986@gmail.com>
2014-01-27 20:36:45 -0600

    got test client working.

--- Project1/Server/testclient.c
+++ Project1/Server/testclient.c
@@ -26,13 +26,14 @@ int main (int argc, char *argv[])
   char *hostname = argv[1];
   char *portString = argv[2];
   unsigned short serverPort = atoi(portString);
+  char ipPrintBuff[200];
   
 
   int sendSock;
 
   sendSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   printf("server host name: %s\n", hostname);
-  printf("server addr: %s\n", getAddress(hostname);
+  printf("server addr: %s\n", inet_ntop(AF_INET, getAddress(hostname), &ipPrintBuff, 200) );
 
 
   struct sockaddr_in servAddr;
@@ -46,6 +47,7 @@ int main (int argc, char *argv[])
   if( send(sendSock, "hello world", strlen("hello world"), 0) < 0)
     perror("send sucks");
 
+  printf("Woo, it works I guess?");
   
 
   return 0;
